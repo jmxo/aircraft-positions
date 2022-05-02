15ad84325f16b602f51c7b986ad0b30a0f32fa39
mo.ismat <mo.ismat@auto1.com>
2022-05-02 17:04:07 +0200

    init commit

---
+++ src/main/java/com/moismat/AircraftPositions/AircraftPositionsApplication.java
@@ -0,0 +1,13 @@
+package com.moismat.AircraftPositions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AircraftPositionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AircraftPositionsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/moismat/AircraftPositions/AircraftPositionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moismat.AircraftPositions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AircraftPositionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
